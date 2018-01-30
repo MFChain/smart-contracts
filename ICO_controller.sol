d7e9e85efc2e5a143a59c5e3ae25d5f8af3001bf
Mykola Ivanov <norfolknorfolks@gmail.com>
2018-01-30 17:44:48 +0200

    First version of contracts

---
+++ ICO_controller.sol
@@ -0,0 +1,160 @@
+pragma solidity ^0.4.19;
+
+import "./SafeMath.sol";
+import "./Ownable.sol";
+import "./MFC_coin.sol";
+import "./ICO_crowdsale.sol";
+import "./Holder.sol";
+
+contract ICO_controller is Ownable{
+
+	using SafeMath for uint256;
+    // The token being sold
+    MFC_Token public token = new MFC_Token();
+
+    // add address for multisig!!
+    Holder holder = new Holder([0x123, 0x123, 0x123], 3);
+
+    // list of buyer are able to participate in ICOs
+    mapping(address=>bool) public buyersWhitelist;
+
+    // store each buyer spent ether
+    mapping(address=>uint256) public buyerSpent;
+
+    // devs&advisors reward
+    mapping(address=>uint256) public devRewards;
+
+    address incentiveProgramAddress = address(0); // TODO
+
+    WhitelistedCrowdsale public privateOffer;
+    WhitelistedCrowdsale public preSale;
+    WhitelistedCrowdsale public crowdsale;
+
+    uint256 public PRIVATE_OFFER_SUPPLY = 7000000 * 1 ether;
+    uint256 public PRE_SALE_SUPPLY = 36000000 * 1 ether;
+    uint256 public CROWDSALE_SUPPLY = 237000000 * 1 ether;
+    uint256 public SOFTCUP = 4720 * 1 ether;
+    uint256 public MAX_DEV_REWARD = 40000000 * 1 ether;
+    uint256 public INCENTIVE_PROGRAM_SUPPORT = 80000000 * 1 ether;
+
+    uint256 public totalDevReward;
+
+    bool public crowdsaleFinished;
+
+    modifier onlyIco() {
+    	require(msg.sender == address(privateOffer) || msg.sender == address(preSale) || msg.sender == address(crowdsale));
+    	_;
+  	}
+
+    function addBuyerToWhitelist(address _buyer) public onlyOwner returns (bool success) {
+    	require(_buyer != address(0));
+    	buyersWhitelist[_buyer] = true;
+    	return true;
+    }
+
+    function removeBuyerFromWhitelist(address _buyer) public onlyOwner returns (bool success) {
+    	require(_buyer != address(0));
+    	buyersWhitelist[_buyer] = false;
+    	return true;
+    }
+
+    function addBuyers(address[] _buyers) external onlyOwner returns (bool success) {
+    	for(uint i=0; i < _buyers.length; i++){
+            addBuyerToWhitelist(_buyers[i]);
+        }
+        return true;
+    }
+
+    function removeBuyers(address[] _buyers) external onlyOwner {
+    	for(uint i=0; i < _buyers.length; i++){
+            removeBuyerFromWhitelist(_buyers[i]);
+        }
+        return true;
+    }
+
+    function isAddressWhitelisted(address buyer) external returns (bool isWhitelisted) {
+    	return buyersWhitelist[buyer];
+    }
+
+    // Adds Devs Token Reward
+    function addDevReward(address _devAddress, uint256 _amount) public onlyOwner returns (bool success) {
+    	require(MAX_DEV_REWARD.sub(totalDevReward) >= _amount);
+    	require(_devAddress != address(0));
+    	totalDevReward = totalDevReward.add(_amount);
+    	devRewards[_devAddress] = devRewards[_devAddress].add(_amount);
+    	return true;
+    }
+
+    function addRewards(address[] _devAddresses, uint256[] _amounts) external onlyOwner returns (bool success) {
+    	require(_devAddresses.length == _amounts.length);
+    	for(uint i=0; i < _amounts.length; i++){
+    		addDevReward(_devAddresses[i], _amounts[i]);
+    	}
+    	returns true;
+    }
+
+    // Create Privaet Offer Sale NOTE: should think about hardwritten rate or parametrized!!!!
+    function startPrivateOffer(uint256 _startTime, uint256 _endTime, uint256 _rate) external onlyOwner {
+    	privateOffer = new WhitelistedCrowdsale(_startTime, _endTime, _rate, address(this), token);
+    	token.transfer(address(privateOffer), PRIVATE_OFFER_SUPPLY);
+    }
+
+    // Create PreSale ICO
+    function startPreSaleIco(uint256 _startTime, uint256 _endTime, uint256 _rate) external onlyOwner {
+    	require(address(privateOffer) != address(0));
+    	require(privateOffer.hasEnded() == true);
+    	preSale = new WhitelistedCrowdsale(_startTime, _endTime, _rate, address(this), token);
+    	token.transfer(address(preSale), PRE_SALE_SUPPLY);
+    	privateOffer.burnRemainingTokens();
+    }
+
+    // Create Crowdsale
+    function startCrowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate) external onlyOwner {
+    	require(address(preSale) != address(0));
+    	require(preSale.hasEnded() == true);
+    	crowdsale = new WhitelistedCrowdsale(_startTime, _endTime, _rate, address(this), token);
+    	token.transfer(address(crowdsale), CROWDSALE_SUPPLY);
+    	preSale.burnRemainingTokens();
+    }
+
+    function finishCrowdsale() external onlyOwner {
+    	require(address(crowdsale) != address(0));
+    	require(crowdsale.hasEnded() == true);
+    	require(crowdsaleFinished == false);
+    	crowdsale.burnRemainingTokens();
+    	uint256 totalSold = privateOffer.getWeiRaised().add(preSale.getWeiRaised().add(crowdsale.getWeiRaised()));
+    	if (totalSold >= SOFTCUP) {
+    		token.transfer(incentiveProgramAddress, INCENTIVE_PROGRAM_SUPPORT);
+    		token.burn(MAX_DEV_REWARD.sub(totalDevReward));
+    		owner.transfer(this.balance.div(2));
+    		address(holder).transfer(this.balance);
+    		holder.activateEscrow();    		
+    	}
+    	crowdsaleFinished = true;
+    	    	
+    }
+
+    // Count each buyer spent amount in case ICO wuoldn't reach SOFTCUP
+    function addBuyerSpent(address _buyer, uint256 _amount) external onlyIco {
+    	buyerSpent[_buyer] = buyerSpent[_buyer].add(_amount);
+    }
+
+    function refund() external returns(bool success) {
+    	require(address(crowdsale) != address(0));
+    	require(crowdsale.hasEnded());
+    	uint256 totalSold = privateOffer.getWeiRaised().add(preSale.getWeiRaised().add(crowdsale.getWeiRaised()));
+    	require(totalSold < SOFTCUP);
+		uint256 amount = buyerSpent[msg.sender];
+		buyerSpent[msg.sender] = 0;
+		msg.sender.transfer(amount);
+		return true;
+
+    }
+
+    //TODO add Randow date check
+    function getDevReward() external {
+    	uint256 amount = devRewards[msg.sender];
+    	devRewards[msg.sender] = 0;
+    	token.transfer(msg.sender, amount);
+    }
+}
---
+++ ICO_crowdsale.sol
@@ -0,0 +1,144 @@
+pragma solidity ^0.4.18;
+
+
+import "./MFC_coin.sol";
+import "./ICO_controller.sol"
+import "./SafeMath.sol";
+import "./Ownable.sol"
+
+
+
+/**
+ * @title Crowdsale
+ * @dev Crowdsale is a base contract for managing a token crowdsale.
+ * Crowdsales have a start and end timestamps, where investors can make
+ * token purchases and the crowdsale will assign them tokens based
+ * on a token per ETH rate. Funds collected are forwarded to a wallet
+ * as they arrive. The contract requires a MintableToken that will be
+ * minted as contributions arrive, note that the crowdsale contract
+ * must be owner of the token in order to be able to mint it.
+ */
+contract WhitelistedCrowdsale is Ownable{
+  using SafeMath for uint256;
+
+  // The token being sold
+  MFC_Token public token;
+
+  // start and end timestamps where investments are allowed (both inclusive)
+  uint256 public startTime;
+  uint256 public endTime;
+
+  // address where funds are collected
+  address public wallet;
+
+  // how many token units a buyer gets per wei
+  uint256 public rate;
+
+  // amount of raised money in wei
+  uint256 public weiRaised;
+
+  ICO_controller private controller;
+
+  /**
+   * event for token purchase logging
+   * @param purchaser who paid for the tokens
+   * @param beneficiary who got the tokens
+   * @param value weis paid for purchase
+   * @param amount amount of tokens purchased
+   */
+  event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount);
+
+
+  function Crowdsale(uint256 _startTime, uint256 _endTime, uint256 _rate, address _wallet, MFC_Token _token) public {
+    require(_startTime >= now);
+    require(_endTime >= _startTime);
+    require(_rate > 0);
+    require(_wallet != address(0));
+    require(_token != address(0));
+
+    startTime = _startTime;
+    endTime = _endTime;
+    rate = _rate;
+    wallet = _wallet;
+    token = _token;
+    controller = msg.sender;
+  }
+
+  // fallback function can be used to buy tokens
+  function () external payable {
+    buyTokens(msg.sender);
+  }
+
+  // low level token purchase function
+  function buyTokens(address beneficiary) public payable {
+    require(beneficiary != address(0));
+    require(validPurchase());
+
+    
+
+    uint256 weiAmount = msg.value;
+
+    // calculate token amount to be created
+    uint256 tokens = getTokenAmount(weiAmount);
+
+    // get avaible for crowdsale token balance
+    uint256 avaibleTokenToSell = token.balanceOf(address(this));
+    require(token <= avaibleTokenToSell);
+
+
+    // update state
+    weiRaised = weiRaised.add(weiAmount);
+
+    token.transfer(beneficiary, tokens);
+    TokenPurchase(msg.sender, beneficiary, weiAmount, tokens);
+    forwardFunds(weiAmount);
+    controller.addBuyerSpent(msg.sender, weiAmount);
+  }
+
+  // @return true if crowdsale event has ended
+  function hasEnded() external view returns (bool) {
+    return now > endTime;
+  }
+
+  // Override this method to have a way to add business logic to your crowdsale when buying
+  function getTokenAmount(uint256 weiAmount) internal view returns(uint256) {
+  	uint256 basicAmount = weiAmount.mul(rate);
+  	uint256 bonusAmount = 0;
+  	if(weiAmount < 5 ether){
+  		
+  	} else if(weiAmount < 10 ether){
+  		bonusAmount =  basicAmount.div(10); // 10%
+  	} else if (weiAmount < 25 ether){
+  		bonusAmount =  basicAmount.mul(15).div(100); // 15%
+  	} else if (weiAmount < 100 ether){
+  		bonusAmount =  basicAmount.mul(17).div(100); // 17%
+  	} else {
+  		bonusAmount =  basicAmount.div(5); // 20%
+  	}
+  	return basicAmount.add(bonusAmount);
+    
+  }
+
+  // send ether to the fund collection wallet
+  // override to create custom fund forwarding mechanisms
+  function forwardFunds(uint256 weiAmount) internal {
+    wallet.transfer(weiAmount);
+  }
+
+  // @return true if the transaction can buy tokens
+  function validPurchase() internal view returns (bool) {
+    bool withinPeriod = now >= startTime && now <= endTime;
+    bool nonZeroPurchase = msg.value != 0;
+    bool isWhitelisted = controller.isAddressWhitelisted(msg.sender);
+    return withinPeriod && nonZeroPurchase && isWhitelisted;
+  }
+
+  function burnRemainingTokens() external onlyOwner {
+  	token.burnAll();
+  }
+
+  function getWeiRaised() external returns(uint256 weiRaised){
+  	return weiRaised;
+  }
+
+}
