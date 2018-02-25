bc384e0939937e095a7ccd27ea63cc921d9a0908
Mykola Ivanov <norfolknorfolks@gmail.com>
2018-02-25 13:26:31 +0200

    Make eth node rpc accessible from the outside of container

--- simulation/tasks.py
+++ simulation/tasks.py
@@ -5,8 +5,8 @@ from invoke import task
 @task
 def run(ctx):
     ctx.run('geth --datadir blchain init blchain/genesis.json')
-    ctx.run('geth --datadir blchain --rpc --nodiscover --mine --minerthreads=1 --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" --networkid 12')
+    ctx.run('geth --datadir blchain --rpc --nodiscover --mine --minerthreads=1 --rpcaddr "0.0.0.0" --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" --networkid 12')
 
 @task
 def run_rinkeby(ctx):
-    ctx.run('geth --syncmode "fast" --cache 512 --datadir "blchain_rinkeby" --rpc --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" --rinkeby')
+    ctx.run('geth --syncmode "fast" --cache 512 --datadir "blchain_rinkeby" --rpcaddr "0.0.0.0" --rpc --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" --rinkeby')
