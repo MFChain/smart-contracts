e535b73110e00f3afd1c7ecee0d614ecfa0e4942
Mykola Ivanov <norfolknorfolks@gmail.com>
2018-02-25 12:38:10 +0200

    add stage_info command to manage.py

--- simulation/manage.py
+++ simulation/manage.py
@@ -1,6 +1,7 @@
 import csv
 import argparse
 import time
+from datetime import datetime
 
 from web3 import Web3, HTTPProvider
 from solc import compile_files
@@ -38,11 +39,13 @@ def get_controller_instance(compiled_source):
     return ico_controller_instance
 
 
-ap = argparse.ArgumentParser()
-
-ap.add_argument('--address', '-a', type=str, help='ICO controller address.', required=True)
-ap.add_argument('command', type=str, choices=['balance', 'whitelist'],
-                help='Command to do')
+def get_ico_instance(address, compiled_source):
+    ico_interface = compiled_source['../contracts/ICO_crowdsale.sol:WhitelistedCrowdsale']
+    ico_contract = w3.eth.contract(
+        abi=ico_interface['abi'],
+        bytecode=ico_interface['bin'])
+    ico_instance = ico_contract(address)
+    return ico_instance
 
 
 def add_address_to_whitelist(address, controller_instance):
@@ -66,6 +69,38 @@ def print_address_balance(address, token_instance):
     print("{} balance: {}".format(address, addr_balance))
 
 
+def print_stage_info():
+    with open('deploy_info.csv', 'rt') as text_file:
+        spamreader = csv.reader(text_file, quoting=csv.QUOTE_MINIMAL)
+        next(spamreader)
+        next(spamreader)
+        next(spamreader)
+        next(spamreader)
+        ico_array = []
+        for name, address in spamreader:
+            ico_array.append((name, get_ico_instance(address, compiled_source)))
+
+    if not ico_array:
+        print("No stage was activated")
+
+    print("Current date: {}\n".format(
+        datetime.utcfromtimestamp(time.time()).strftime('%Y-%m-%dT%H:%M:%SZ'))
+    )
+    for name, ico_instance in ico_array:
+        print("Stage: {}\n  Address: {}\n  Has ended: {}\n  Start date: {}\n  End date: {}".format(
+            name, ico_instance.address, ico_instance.call().hasEnded(),
+            datetime.utcfromtimestamp(ico_instance.call().startTime()).strftime('%Y-%m-%dT%H:%M:%SZ'),
+            datetime.utcfromtimestamp(ico_instance.call().endTime()).strftime('%Y-%m-%dT%H:%M:%SZ'),
+        ))
+        print("  Wei raised: {}\n".format(ico_instance.call().getWeiRaised()))
+
+
+ap = argparse.ArgumentParser()
+
+ap.add_argument('--address', '-a', type=str, help='ICO controller address.')
+ap.add_argument('command', type=str, choices=['balance', 'whitelist', 'stage_info'],
+                help='Command to do')
+
 if __name__ == '__main__':
     args = vars(ap.parse_args())
     address = args['address']
@@ -73,13 +108,6 @@ if __name__ == '__main__':
 
     w3 = Web3(HTTPProvider('http://127.0.0.1:8545'))
     w3.personal.unlockAccount(w3.eth.accounts[0], '1')
-
-    with open('deploy_info.csv', 'rt') as text_file:
-        spamreader = csv.reader(text_file, quoting=csv.QUOTE_MINIMAL)
-        next(spamreader)
-        next(spamreader)
-        next(spamreader)
-        token_address = next(spamreader)[1]
 
     compiled_source = compile_files(["../contracts/ICO_controller.sol"])
 
@@ -90,7 +118,5 @@ if __name__ == '__main__':
         print_address_balance(address, token_instance)
     elif command == 'whitelist':
         add_address_to_whitelist(address, controller_instance)
-
-
-
-
+    elif command == 'stage_info':
+        print_stage_info()
