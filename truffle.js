d46939e893cb79f21f7adad3e2698e01df2bb4d3
Mykola Ivanov <norfolknorfolks@gmail.com>
2018-03-03 12:39:23 +0200

    Rename MFC token to MFX Change require to assert in some functions Add increaseTime function for date testing Minor changes

--- truffle.js
+++ truffle.js
@@ -1,4 +1,11 @@
 module.exports = {
+    networks: {
+        development: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*" // Match any network id
+        }
+    },
     mocha: {
         useColors: true
     },
