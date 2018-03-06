1402d3c832fe67db56fbd66c80728918b0f3599a
Mykola Ivanov <norfolknorfolks@gmail.com>
2018-03-06 17:46:20 +0200

    Add check if checksomeAddress

--- test/m_f_c_coin.js
+++ test/m_f_c_coin.js
@@ -1,7 +1,7 @@
 var token = artifacts.require("MFC_Token");
 
 contract('MFC_Token', function(accounts) {
-    it("should specify totalSupply as 500000000000000000000000000 MFC_Token and put all tokens in the first account", function() {
+    it("should specify totalSupply as 507000000000000000000000000 MFC_Token and put all tokens in the first account", function() {
         var balance_value;
         var totalSupply_value;
         var owner = accounts[0];
@@ -15,8 +15,8 @@ contract('MFC_Token', function(accounts) {
       }).then(function(totalSupply) {
           totalSupply_value = totalSupply.valueOf();
       }).then(function() {
-          assert.equal(balance_value, 500000000000000000000000000, "500000000000000000000000000 wasn't in the first account");
-          assert.equal(totalSupply_value, 500000000000000000000000000, "totalSupply is not 500000000000000000000000000 MFC_Token");
+          assert.equal(balance_value, 507000000000000000000000000, "507000000000000000000000000 wasn't in the first account");
+          assert.equal(totalSupply_value, 507000000000000000000000000, "totalSupply is not 507000000000000000000000000 MFC_Token");
       });
   });
 });
