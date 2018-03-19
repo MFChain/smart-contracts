b946434f3001b3bc5c26c54eae83ea919d003677
Boris Serebrov <serebrov@gmail.com>
2018-03-19 18:37:57 +0200

    Add solidity-coverage

---
+++ .solcover.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // port: 6545,
+    testrpcOptions: '--accounts 1000 --port 8555 --gasLimit 17592186044415',
+    //testCommand: 'mocha --timeout 5000',
+    //norpc: true,
+    // dir: './secretDirectory',
+    // copyPackages: ['zeppelin-solidity'],
+    // skipFiles: ['Routers/EtherRouter.sol']
+};
