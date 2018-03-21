1bd0b2ce73dc04935377ff9fb93a457b6e2474db
Boris Serebrov <serebrov@gmail.com>
2018-03-21 17:43:25 +0200

    Adjust solcover

--- .solcover.js
+++ .solcover.js
@@ -1,6 +1,6 @@
 module.exports = {
     // port: 6545,
-    testrpcOptions: '--accounts 1000 --port 8555 --gasLimit 17592186044415',
+    testrpcOptions: '--accounts 1000 -e 100000 --port 8555', // --port 8555 --gasLimit 17592186044415',
     //testCommand: 'mocha --timeout 5000',
     //norpc: true,
     // dir: './secretDirectory',
