5df43ef24a4730fe9e021467a0cb2ae9c45c47ff
Artem <artemiy.baranov@gmail.com>
2018-03-22 19:55:05 +0200

    create log dir

--- user_run/user_run.sh
+++ user_run/user_run.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+CHECK=0
+INFO_CSV="deploy_info.csv"
+BDDI_LOG="bd_deploy_info.log"
+OWNERS_OF_MULTISIG="0x145CC078183A8A38d03e39086956fA759505C987,0xf5fA1aAC73e222093218cd14B9d757ba7B853423,0x748D8a3805Be42352C82192359D1096eCdFfb3e4"
+ESCROW="0xb36A2bfB9232048Acf5c229BFc118D2c8FdCaDF3"
+CSTMR="0x87544f8D45A31a317F197189Ffd5e915D2d46E58"
+MLCE_LOG="current_event.txt"
+
 C_PATH=`pwd`
 cd $C_PATH/..
 M_PATH=`pwd`
 cd simulation/
 
-CHECK=0
-INFO_CSV="deploy_info.csv"
-BDDI_LOG="bd_deploy_info.log"
-OWNERS_OF_MULTISIG="0x145CC078183A8A38d03e39086956fA759505C987, \
-	0xf5fA1aAC73e222093218cd14B9d757ba7B853423, \
-	0x748D8a3805Be42352C82192359D1096eCdFfb3e4"
-ESCROW="0xb36A2bfB9232048Acf5c229BFc118D2c8FdCaDF3"
-CSTMR="0x87544f8D45A31a317F197189Ffd5e915D2d46E58"
+[ ! -d $C_PATH/log/ ] && mkdir $C_PATH/log
+touch $C_PATH/log/$MLCE_LOG && tail -f $C_PATH/log/$MLCE_LOG &
 
 function t_Log(){
 	printf "[+] `date` -> $*\n"
@@ -33,8 +35,8 @@ function t_Docker_Compose(){
 function r_Deploy_Contracts(){
 	python3 deploy_contracts.py -a $OWNERS_OF_MULTISIG -r 2 -e $ESCROW
 	if [ -f "${INFO_CSV}" ];then
-		echo -e "\n\t = = = $(date) = = =\n" >> $BDDI_LOG;
-		cat ${INFO_CSV} >> $BDDI_LOG;
+		echo -e "\n\t = = = $(date) = = =\n" >> $C_PATH/log/$BDDI_LOG;
+		cat ${INFO_CSV} >> $C_PATH/log/$BDDI_LOG;
 	else
 		CHECK=`expr $CHECK + 1`
 		t_Log "File ${INFO_CSV} doesn't exists."
@@ -46,7 +48,7 @@ function r_Start_Ico_Stage(){
 	ICO_TSTART=`python3 ${M_PATH}/user_run/unix_time_generate.py | awk '{print $1}'` #+5
 	ICO_TEND=`python3 ${M_PATH}/user_run/unix_time_generate.py | awk '{print $2}'` #+20
 	python3 start_ico_stage.py -e $CSTMR -s $ICO_TSTART -d $ICO_TEND -t $ICO_TYPE
-	cat $INFO_CSV | tail -1 >> $BDDI_LOG
+	cat $INFO_CSV | tail -1 >> $C_PATH/log/$BDDI_LOG
 }
 
 function t_Read_Deploy_Info(){
@@ -55,13 +57,13 @@ function t_Read_Deploy_Info(){
 }
 
 if [ -f 'start_ico_stage.py' ];then
-	#t_Docker_Compose;
-	r_Deploy_Contracts;
-	r_Start_Ico_Stage private_offer;
-	t_Read_Deploy_Info;
+	##t_Docker_Compose >> $C_PATH/log/$MLCE_LOG
+	r_Deploy_Contracts >> $C_PATH/log/$MLCE_LOG
+	r_Start_Ico_Stage private_offer >> $C_PATH/log/$MLCE_LOG
+	t_Read_Deploy_Info >> $C_PATH/log/$MLCE_LOG
 else
 	CHECK=`expr $CHECK + 1`
 	t_Log "Check the current directory."
 fi
 
-echo $CHECK
+t_Log $CHECK
