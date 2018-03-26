80341192e27ea7a64cc2979537bc28a255d88075
lontkovsky <lontkovsky@gmail.com>
2018-03-26 15:07:57 +0300

    added some changes to test_crowdsale.js

--- test/test_crowdsale.js
+++ test/test_crowdsale.js
@@ -1,11 +1,7 @@
-"use strict";
-
 const MFC_Token = artifacts.require("MFC_Token");
 const WhitelistedCrowdsale = artifacts.require("WhitelistedCrowdsale");
 
 contract('ICO_crowdsale test', async (accounts) => {
-    /* Task 41 - Create test for ICO_crowdsale getTokenAmount() */
-
     const owner = accounts[0];
     const user = accounts[1];
 
@@ -17,10 +13,10 @@ contract('ICO_crowdsale test', async (accounts) => {
 
     const rate = 10;
 
-    const testWeiAmount = ether_0;
+    let weiAmount = ether_0;
 
     it('should add nothing to bonus amount for with 0 <= weiAmount < 10 ', async function () {
-        let weiAmount = ether_5;
+        weiAmount = ether_5;
 
         let basicAmount = weiAmount * rate;
         let bonusAmount = 0;
@@ -34,11 +30,9 @@ contract('ICO_crowdsale test', async (accounts) => {
         await web3.eth.sendTransaction({from: user, to: owner, value: weiAmount, gasLimit: 6721975, gasPrice: 20000000000});
 
         balance = await web3.eth.getBalance(owner);
-
         let balance_after = web3.fromWei(balance.toNumber(), 'ether');
 
         let amount = await web3.toWei((balance_after - balance_before), 'ether');
-
         let tokenAmount = await contract.getTokenAmount(amount);
 
         let res1 = await web3.fromWei(result, 'ether');
@@ -48,7 +42,7 @@ contract('ICO_crowdsale test', async (accounts) => {
     });
 
     it('should add 10% to bonus amount for 10 <= weiAmount < 25', async function () {
-        let weiAmount = ether_10;
+        weiAmount = ether_10;
 
         let basicAmount = weiAmount * rate;
         let bonusAmount = basicAmount * 0.1;
@@ -62,11 +56,9 @@ contract('ICO_crowdsale test', async (accounts) => {
         await web3.eth.sendTransaction({from: user, to: owner, value: weiAmount, gasLimit: 6721975, gasPrice: 20000000000});
 
         balance = await web3.eth.getBalance(owner);
-
         let balance_after = web3.fromWei(balance.toNumber(), 'ether');
 
         let amount = await web3.toWei((balance_after - balance_before), 'ether');
-
         let tokenAmount = await contract.getTokenAmount(amount);
 
         let res1 = await web3.fromWei(result, 'ether');
@@ -76,7 +68,7 @@ contract('ICO_crowdsale test', async (accounts) => {
     });
 
     it('should add 15% to bonus amount for  25 <= weiAmount < 100', async function () {
-        let weiAmount = ether_25;
+        weiAmount = ether_25;
 
         let basicAmount = weiAmount * rate;
         let bonusAmount = basicAmount * 0.15;
@@ -90,11 +82,9 @@ contract('ICO_crowdsale test', async (accounts) => {
         await web3.eth.sendTransaction({from: user, to: owner, value: weiAmount, gasLimit: 6721975, gasPrice: 20000000000});
 
         balance = await web3.eth.getBalance(owner);
-
         let balance_after = web3.fromWei(balance.toNumber(), 'ether');
 
         let amount = await web3.toWei((balance_after - balance_before), 'ether');
-
         let tokenAmount = await contract.getTokenAmount(amount);
 
         let res1 = await web3.fromWei(result, 'ether');
@@ -104,7 +94,7 @@ contract('ICO_crowdsale test', async (accounts) => {
     });
 
     it('should add 20% to bonus amount for weiAmount >= 100  ', async function () {
-        let weiAmount = ether_100;
+        weiAmount = ether_100;
 
         let basicAmount = weiAmount * rate;
         let bonusAmount = basicAmount * 0.2;
@@ -118,11 +108,9 @@ contract('ICO_crowdsale test', async (accounts) => {
         await web3.eth.sendTransaction({from: user, to: owner, value: weiAmount, gasLimit: 6721975, gasPrice: 20000000000});
 
         balance = await web3.eth.getBalance(owner);
-
         let balance_after = web3.fromWei(balance.toNumber(), 'ether');
 
         let amount = await web3.toWei((balance_after - balance_before), 'ether');
-
         let tokenAmount = await contract.getTokenAmount(amount);
 
         let res1 = await web3.fromWei(result, 'ether');
