5bc7f6765dafa13b007a6090301963618c6654a5
Mykola Ivanov <norfolknorfolks@gmail.com>
2018-03-28 23:35:51 +0300

    In some cases getTransaction could return None instead of Exception - fix

--- simulation/utils.py
+++ simulation/utils.py
@@ -8,7 +8,7 @@ def wait_for_tx(tx_hash, web3, delay=5, wait_message=None):
     while True:
         try:
             reciept = web3.eth.getTransactionReceipt(tx_hash)
-            if reciept == None: raise Exception()
+            if reciept == None: raise Exception('Transaction still pending')
             break
         except:
             if wait_message:
