a6bf12f5db218028f05a0bde5184eea8726fe3d9
Mykola Ivanov <norfolknorfolks@gmail.com>
2018-03-31 00:23:00 +0300

    fix address listening

--- simulation/tasks.py
+++ simulation/tasks.py
@@ -6,12 +6,12 @@ from invoke import task
 def run(ctx):
     ctx.run('yes | geth --datadir blchain removedb')
     ctx.run('geth --datadir blchain init blchain/genesis.json')
-    ctx.run('geth --datadir blchain --rpc --nodiscover --mine --minerthreads=1 --rpcaddr "121.0.0.1" --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" --networkid 12')
+    ctx.run('geth --datadir blchain --rpc --nodiscover --mine --minerthreads=1 --rpcaddr "127.0.0.1" --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" --networkid 12')
 
 @task
 def run_rinkeby(ctx):
-    ctx.run('geth --syncmode "fast" --cache 512 --datadir "blchain_rinkeby" --rpcaddr "121.0.0.1" --rpc --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" --rinkeby')
+    ctx.run('geth --syncmode "fast" --cache 512 --datadir "blchain_rinkeby" --rpcaddr "127.0.0.1" --rpc --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal" --rinkeby')
 
 @task
 def run_mainnet(ctx):
-    ctx.run('geth --syncmode "fast" --cache 512 --datadir "blchain_mainnet" --rpcaddr "121.0.0.1" --rpc --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal"')
+    ctx.run('geth --syncmode "fast" --cache 512 --datadir "blchain_mainnet" --rpcaddr "127.0.0.1" --rpc --rpccorsdomain "*" --rpcapi "db,eth,net,web3,personal"')
