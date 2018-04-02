6b7c5661f5d5005216c226ac7f76768d2ff0b5a1
lontkovsky <lontkovsky@gmail.com>
2018-04-02 13:10:29 +0300

    t_126_test_Token_holder.escrowTokenTo

---
+++ test/test_token_holder.js
@@ -0,0 +1,41 @@
+const Token = artifacts.require("MFC_Token");
+const tokenHolder = artifacts.require("TokenHolder");
+const ICO_controller = artifacts.require("ICO_controller");
+
+contract('Token_Holder tests escrowTokensTo()', async function(accounts) {
+    /* Task 126 - Create test for Token_Holder escrowTokensTo() */
+
+    it("Test Token_Holder escrowTokensTo()", async function() {
+        let owner1 = accounts[1];
+        let owner2 = accounts[2];
+        let owner3 = accounts[3];
+        let escrowAddress = accounts[4];
+
+        let token = await Token.new();
+
+        let tokenHolderContract = await tokenHolder.new(
+            [owner1,owner2,owner3], 3, token.address
+        );
+
+        token.transfer(tokenHolderContract.address, 10);
+
+        let balance = await token.balanceOf(tokenHolderContract.address);
+        assert.equal(10, balance.toNumber());
+
+        assert.equal(0, await token.balanceOf(escrowAddress));
+
+        await tokenHolderContract.escrowTokensTo(escrowAddress, {'from': owner1});
+        assert.equal(0, await token.balanceOf(escrowAddress));
+
+        await tokenHolderContract.escrowTokensTo(escrowAddress, {'from': owner2});
+        assert.equal(0, await token.balanceOf(escrowAddress));
+
+        await tokenHolderContract.escrowTokensTo(escrowAddress, {'from': owner3});
+
+        let value = await token.balanceOf(escrowAddress);
+
+        assert.equal(0, await token.balanceOf(tokenHolderContract.address));
+        assert.equal(10, value.toNumber());
+
+    });
+});
